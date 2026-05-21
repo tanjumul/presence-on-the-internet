f16f1d542aa0666ae607251015043461ed604715
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-21 16:17:23 +0600

    setting height and installing scrollbar

--- tanjumul-alom/src/App.jsx
+++ tanjumul-alom/src/App.jsx
@@ -1,5 +1,5 @@
 const App = ()=>{ 
-  return <h1 className="text-3xl font-bold">Hello</h1>
+  return <h1 className="text-3xl font-bold p-10 bg-linear-to-r from-red-500 to-yellow-400 bg-clip-text text-transparent">Hello, Tanjumul Alom Siam</h1>
 }
 
 export default App; 
